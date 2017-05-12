61b516913244c374fe20eef5efaa5b8b869d8dde
Shubham Chaudhary <me@shubhamchaudhary.in>
2017-05-12 12:31:37 +0530

    Be explicit about missing queue vs unknown type

--- qmon/status.py
+++ qmon/status.py
@@ -23,9 +23,8 @@ def get_items_in_queue(queue_name, redis_conn, default=None):
         length_op = redis_conn.llen
     elif type_name == 'set':
         length_op = redis_conn.scard
+    elif type_name == 'none' and default is not None:
+        length_op = lambda _: default
     else:
-        if default is not None:
-            length_op = lambda _: default
-        else:
-            raise ValueError('Queue empty or Unknown type_name "{}" of queue "{}"'.format(type_name, queue_name))
+        raise ValueError('Queue empty or Unknown type_name "{}" of queue "{}"'.format(type_name, queue_name))
     return length_op(queue_name)
